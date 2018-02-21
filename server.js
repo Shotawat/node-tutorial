7269a737c077698b20c756dbb5c01722b74d6bb7
Shotawat <tawat.ka@gmail.com>
2018-02-21 20:33:18 +0700

    update server.js version 1.4

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! Version 1.4'))
 app.get('/Sho', (req, res) => res.send('Hello Sho in the World of Node-js and Docker!!!!'))
 app.get('/Yod', (req, res) => res.send('Hello Yod!!!!'))
 app.get('/Yo', (req, res) => res.send('Hello Yo!!!!'))
+app.get('/Sho/1', (req, res) => res.send('Hello Sho Yeahhh!!!!'))
+
+
 
 app.listen(3310     , () => console.log('Example app listening on port 3310!!'))
