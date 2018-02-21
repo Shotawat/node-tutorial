62d2e558fd771220fd0b045cdbc4a7f5157e5a1e
Shotawat <tawat.ka@gmail.com>
2018-02-21 17:57:29 +0700

    update server.js file again

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/Sho', (req, res) => res.send('Hello Sho in the World of Node-js and Docker!!!!'))
+app.get('/Yo', (req, res) => res.send('Hello Yo!!!!'))
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
