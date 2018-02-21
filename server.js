0093b703951e9fa5af8351923f3dde6bad99bb8e
Shotawat <tawat.ka@gmail.com>
2018-02-21 18:28:57 +0700

    update pot in serverjs

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/Sho', (req, res) => res.send('Hello Sho in the World of Node-js and Docker!!!!'))
 app.get('/Yo', (req, res) => res.send('Hello Yo!!!!'))
 
-app.listen(8080, () => console.log('Example app listening on port 8080!'))
+app.listen(3310, () => console.log('Example app listening on port 8080!'))
